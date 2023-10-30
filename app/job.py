542e9cb45c719fb20da8ccb8cd3e9d8caa8e62c2
Dmitry Misharov <dmitry@openssl.org>
2023-10-30 10:09:18 +0100

    add provision_vm and destroy_vm wrappers to use in threads

---
+++ app/job.py
@@ -0,0 +1,19 @@
+from cloud import cloud
+import threading
+from models import VmTemplate
+from models import WorkflowJobWebHook
+
+
+events = {}
+
+
+def queue(webhook: WorkflowJobWebHook, vm_template: VmTemplate) -> None:
+    event = events[webhook.job_url] = threading.Event()
+    cloud.provision_vm(webhook, vm_template)
+    event.wait()
+
+
+def complete(webhook: WorkflowJobWebHook) -> None:
+    cloud.destroy_vm(webhook)
+    event = events[webhook.job_url]
+    event.set()
